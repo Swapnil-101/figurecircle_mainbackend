7ceb030cf94f89a6a583d69a9bd77ef4b2a75f56
Swapnil-101 <74921288+Swapnil-101@users.noreply.github.com>
2025-06-24 22:04:15 +0530

    sdawd

--- app.py
+++ app.py
@@ -449,7 +449,10 @@ def register():
     session.commit()
     session.close()
 
-    return jsonify({"message": "User registered successfully"}), 201
+    return jsonify({
+        "message": "User registered successfully",
+        "register": True
+    }), 201
 
 
 @app.route('/login', methods=['POST'])
